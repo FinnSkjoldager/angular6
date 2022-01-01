ca2245de0b7d6b670a8a1c96069eddd3642379dd
Finn Skjoldager <finn.skjold@gmail.com>
2022-01-02 00:35:32 +0100

    make it better

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/finnsk'));
+app.use(express.static(__dirname + '/dist/angular3'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
-'/dist/finnsk/index.html'));});
+'/dist/angular3/index.html'));});
 app.listen(process.env.PORT || 8080);
